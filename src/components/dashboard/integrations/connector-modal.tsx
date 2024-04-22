8876c26758f71771a0c54fdf00732e84e0fee2dc
atanu2110 <66422706+atanu2110@users.noreply.github.com>
2024-04-22 22:29:39 +0530

    first commit

---
+++ src/components/dashboard/integrations/connector-modal.tsx
@@ -0,0 +1,58 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import { PlugsConnected  as PlugsConnectedIcon } from '@phosphor-icons/react/dist/ssr/PlugsConnected';
+import TextField from '@mui/material/TextField';
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+const style = {
+  position: 'absolute' as 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
+
+export default function ConnectorModal() {
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+
+  
+  return (
+    <div>
+      <Button startIcon={<PlugsConnectedIcon fontSize="var(--icon-fontSize-md)" />} variant="contained" onClick={handleOpen}>
+            Connect
+          </Button>
+          <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Connection Details"}
+        </DialogTitle>
+        <DialogContent>
+        <TextField id="outlined-basic" label="Outlined" variant="outlined" />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose}>Cancel</Button>
+          <Button onClick={handleClose} autoFocus>
+            Submit
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </div>
+  );
+}
