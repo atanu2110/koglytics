1d53299106a4f69cd2a6027610fcc7bd9066dcd7
atanu2110 <66422706+atanu2110@users.noreply.github.com>
2024-04-24 21:27:13 +0530

    added distribution table

--- src/app/dashboard/profiling/page.tsx
+++ src/app/dashboard/profiling/page.tsx
@@ -63,30 +63,49 @@ export default function Page(): React.JSX.Element {
   const [data, setData] = useState(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [duplicateCount, setDuplicateCount] = useState(0);
+  const [distribution, setDistribution] = useState('');
 
   const handleChange = (event: SelectChangeEvent) => {
     setTable(event.target.value as string);
     setLoading(true);
-    fetch('http://localhost:9009/api/v1/profile/d/schema')
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Failed to fetch data');
-        }
-        return response.json();
-      })
-      .then(jsonData => {
-        setData(jsonData);
-        setLoading(false);
-        console.log(jsonData);
-      })
-      .catch(error => {
-        setError(error.message);
-        setLoading(false);
-      });
+    // fetch('http://localhost:9009/api/v1/profile/d/schema')
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error('Failed to fetch data');
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(jsonData => {
+    //     setData(jsonData);
+    //     setLoading(false);
+    //     console.log(jsonData);
+    //   })
+    //   .catch(error => {
+    //     setError(error.message);
+    //     setLoading(false);
+    //   });
+
+    Promise.all([
+        fetch('http://localhost:9009/api/v1/profile/d/schema').then(response => response.json()),
+        fetch('http://localhost:9009/api/v1/profile/s/duplicate/count').then(response => response.json()),
+        fetch('http://localhost:9009/api/v1/profile/s/distribution').then(response => response.json())
+      ])
+        .then(([data1Response, data2Response, data3Response]) => {
+            setData(data1Response);
+            setDuplicateCount(data2Response);
+            setDistribution(data3Response);
+          setLoading(false);
+        })
+        .catch(error => {
+          setError(error.message);
+          setLoading(false);
+        });
   };
 
   const handleCellClick = (e) => {
     alert(e.target.textContent);
+    console.log(distribution[3]);
 }
 
   return (
@@ -146,26 +165,49 @@ export default function Page(): React.JSX.Element {
 
           {(table != '' && data != null) &&
               <div>
-                    <Grid container spacing={3}>
-              <Grid lg={3} sm={6} xs={12}>
-                  <Card>
-                      <CardContent>
-                          <Stack spacing={2}>
-                              <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
-                                  <Stack spacing={1}>
-                                      <Typography color="text.secondary" gutterBottom variant="overline">
-                                          Total Rows
-                                      </Typography>
-                                      <Typography variant="h4">{data.rows}</Typography>
+                  <Grid container spacing={3}>
+                      <Grid lg={3} sm={6} xs={12}>
+                          <Typography variant="h4">Data Quality <Select
+                              labelId="demo-simple-select-label"
+                              id="demo-simple-select"
+                              value={table}
+                              label="Select Table"
+                              onChange={handleChange}
+                          >
+                              {/* <MenuItem value={10}>Ten</MenuItem>
+                      <MenuItem value={20}>Twenty</MenuItem>
+                      <MenuItem value={30}>Thirty</MenuItem> */}
+                              {test.map((value: any) => (
+                                  <MenuItem
+                                      key={value}
+                                      value={value}>
+                                      {value}
+                                  </MenuItem>
+                              ))}
+                          </Select></Typography>
+                      </Grid>
+                  </Grid>
+                  <br />
+                  <Grid container spacing={3}>
+                      <Grid lg={3} sm={6} xs={12}>
+                          <Card>
+                              <CardContent>
+                                  <Stack spacing={2}>
+                                      <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
+                                          <Stack spacing={1}>
+                                              <Typography color="text.secondary" gutterBottom variant="overline">
+                                                  Total Rows
+                                              </Typography>
+                                              <Typography variant="h4">{data.rows}</Typography>
+                                          </Stack>
+                                          <Avatar sx={{ backgroundColor: 'var(--mui-palette-warning-main)', height: '56px', width: '56px' }}>
+                                              <ListBulletsIcon fontSize="var(--icon-fontSize-lg)" />
+                                          </Avatar>
+                                      </Stack>
                                   </Stack>
-                                  <Avatar sx={{ backgroundColor: 'var(--mui-palette-warning-main)', height: '56px', width: '56px' }}>
-                                      <ListBulletsIcon fontSize="var(--icon-fontSize-lg)" />
-                                  </Avatar>
-                              </Stack>                    
-                          </Stack>
-                      </CardContent>
-                  </Card>
-              </Grid>
+                              </CardContent>
+                          </Card>
+                      </Grid>
               <Grid lg={3} sm={6} xs={12}>
               <Card>
                       <CardContent>
@@ -185,6 +227,25 @@ export default function Page(): React.JSX.Element {
                       </CardContent>
                   </Card>
               </Grid>
+              <Grid lg={3} sm={6} xs={12}>
+              <Card>
+                      <CardContent>
+                          <Stack spacing={2}>
+                              <Stack direction="row" sx={{ alignItems: 'flex-start', justifyContent: 'space-between' }} spacing={3}>
+                                  <Stack spacing={1}>
+                                      <Typography color="text.secondary" gutterBottom variant="overline">
+                                          Duplicate Rows
+                                      </Typography>
+                                      <Typography variant="h4">{duplicateCount}</Typography>
+                                  </Stack>
+                                  <Avatar sx={{ backgroundColor: 'var(--mui-palette-warning-main)', height: '56px', width: '56px' }}>
+                                  <ListBulletsIcon fontSize="var(--icon-fontSize-lg)" />
+                                  </Avatar>
+                              </Stack>                    
+                          </Stack>
+                      </CardContent>
+                  </Card>
+              </Grid>
           </Grid>
           <br/>
           {/* <CustomersTable
@@ -195,7 +256,7 @@ export default function Page(): React.JSX.Element {
           /> */}
 
                   <TableContainer component={Paper}>
-                      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+                      <Table sx={{ minWidth: 300 }} aria-label="simple table">
                           <TableHead>
                               <TableRow>
                                   <TableCell>Name</TableCell>
@@ -210,8 +271,9 @@ export default function Page(): React.JSX.Element {
                                       key={row.name}
                                       sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
                                   >
-                                      <TableCell component="th" scope="row" onClick={handleCellClick}>
-                                          {row.name}
+                                      <TableCell component="th" scope="row" onClick={handleCellClick}
+                                      style={{backgroundColor:'#dfe1e4', color: 'black',}}>
+                                      <u><strong>{row.name}</strong></u>
                                       </TableCell>
                                       <TableCell align="right">{row.type}</TableCell>
                                       <TableCell align="right">{row.nullable ? 'true' : 'false'}</TableCell>
@@ -221,7 +283,31 @@ export default function Page(): React.JSX.Element {
                           </TableBody>
                       </Table>
                   </TableContainer>
+                  <br/>
 
+                  <Grid lg={3} sm={6} xs={12}>
+                  <Typography variant="h4">Table Distribution</Typography><br/>
+                      <TableContainer component={Paper}>
+                          <Table>
+                              <TableHead>
+                                  <TableRow>
+                                      {distribution.columns.map((column, index) => (
+                                          <TableCell key={index}>{column}</TableCell>
+                                      ))}
+                                  </TableRow>
+                              </TableHead>
+                              <TableBody>
+                                  {distribution.rows.map((row, rowIndex) => (
+                                      <TableRow key={rowIndex}>
+                                          {distribution.columns.map((column, colIndex) => (
+                                              <TableCell key={colIndex}>{row[column]}</TableCell>
+                                          ))}
+                                      </TableRow>
+                                  ))}
+                              </TableBody>
+                          </Table>
+                      </TableContainer>
+                  </Grid>
 
               </div>
           }
